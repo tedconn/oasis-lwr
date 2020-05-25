dfe1d44ce9d41d02d2e6b363f2a6b8ad6581834f
Ted <tconn@salesforce.com>
2020-05-25 14:40:53 +0200

    creating bootstrap app

--- src/modules/x/header/header.js
+++ src/modules/x/header/header.js
@@ -1,5 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 
 export default class Header extends LightningElement {
 
+    active = null;
+
+    @wire(CurrentPageReference) currentPageChange(data) {
+        this.active = data.attributes.name;
+        this.setActive(data.attributes.name);
+    };
+
+    renderedCallback() {
+        this.setActive(this.active);
+    }
+
+    setActive(active) {
+        this.template.querySelectorAll('x-header-link').forEach(el => {
+            console.log(`setting ${el.route} to ${(el.route == active)}`)
+            el.active = (el.route == active);
+        })
+    }
 }
