46271607ff1846a3ab4dbd08ffed7b906a535c74
Ted Conn <tconn@salesforce.com>
2020-07-26 17:35:45 +0200

    fix: manually passing in desired port

--- webruntime-app.config.js
+++ webruntime-app.config.js
@@ -2,12 +2,12 @@ module.exports = {
     projectDir: __dirname,
     buildDir: 'dist',
     moduleDir: 'src/modules',
-    watch: true,
+    watch: false,
     app: {
         defaultTemplate: 'src/index.html',
     },
     server: {
-        port: +process.env.PORT || 3001,
+        port: +process.env.PORT || 3001, // this isn't even respected, sigh
         basePath: '',
         contentSecurityPolicy: {
             directives: {
