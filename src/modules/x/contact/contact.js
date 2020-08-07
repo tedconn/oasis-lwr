5303b5d2024c0c23ff30bd837a3aa04bddb9f253
Ted Conn <tconn@salesforce.com>
2020-08-07 12:31:04 +0200

    fix: added heroku target

--- src/modules/x/contact/contact.js
+++ src/modules/x/contact/contact.js
@@ -1,5 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class Contact extends LightningElement {
-    
+    connectedCallback() {
+        console.log(window.location.search);
+    }
 }
