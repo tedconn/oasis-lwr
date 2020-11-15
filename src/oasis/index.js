70a73db39d563905a75066be18271fa0d25d7d03
Ted Conn <tconn@salesforce.com>
2020-11-15 19:55:42 +0100

    fix: add oasis files from npm

--- src/oasis/index.js
+++
@@ -1,1 +0,0 @@
-export * as OasisScript from '@salesforce/oasis';
--- webruntime-app.config.js
+++ webruntime-app.config.js
@@ -11,7 +11,7 @@ module.exports = {
         basePath: '',
         contentSecurityPolicy: {
             directives: {
-                scriptSrc: ['cdn.evgnet.com']
+                scriptSrc: ['www.googletagmanager.com']
             },
             loose: true,
         }
